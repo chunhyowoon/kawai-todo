f3a257d1a5f3719a5f2d61238ecc54b7f1b2e86f
chunhyowoon <omaticxx@naver.com>
2020-08-12 16:24:27 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import { StatusBar } from "expo-status-bar";
-import React from "react";
+import React, {Component} from "react";
 import ToDo from "./ToDo";
 import {
   StyleSheet,
@@ -12,60 +12,72 @@ import {
 } from "react-native";
 
 const { height, width } = Dimensions.get("window");
-export default function App() {
-  const [startState, updateState] = React.useState("first");
-  function changeText(text){
-    updateState(text);
-  }
-  return (
-    <View style={styles.container}>
-      <StatusBar barStyle="light-content" />
-      <Text style={styles.title}>Kawai To Do</Text>
-      <View stylea={styles.card}>
-        <TextInput style={styles.input} placeholder={"inputText"} value={startState} onChangeText={changeText}></TextInput>
-        <ScrollView contentContainerStyle={styles.todos}>
-          <ToDo></ToDo>
-        </ScrollView>
-      </View>
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#f23657",
-    alignItems: "center",
-  },
-  title: {
-    color: "white",
-    fontSize: 30,
-    marginTop: 50,
-    fontWeight: "200",
-  },
-  card: {
-    backgroundColor: "white",
-    flex: 1,
-    width: width - 25,
-    borderTopLeftRadius: 10,
-    borderTopRightRadius: 10,
-    ...Platform.select({
-      ios: {
-        shadowColor: "rgb(50,50,50)",
-        shadowOpacity: 0.5,
-        shadowRadius: 5,
-        shadowOffset: {
-          height: -1,
-          width: 0,
-        },
-      },
-      android: {
-        elevation: 3,
-      },
-    }),
-  },
-  input: {},
-  toDos:{
-    alignItems:"center"
+export default class App extends Component {
+  state = {
+    newTodo: ""
   }
-});
+  render(){
+    const {newTodo} = this.state;
+    return (
+      <View style={styles.container}>
+        <StatusBar barStyle="light-content"></StatusBar>
+        <Text style={styles.title}>Kawai To Do</Text>
+        <View style={styles.card}>
+          <TextInput style={styles.input} placeholder={"inputText"} value={newTodo} onChangeText={this._controllNewTodo}></TextInput>
+          <ScrollView contentContainerStyle={styles.todos}>
+            <ToDo text={"hello i'm to do"}></ToDo>
+          </ScrollView>
+        </View>
+      </View>
+    );
+  }
+  _controllNewTodo = (text) => {
+    this.setState({
+      newTodo : text
+    })
+  }
+}
+  const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: "#f23657",
+      alignItems: "center",
+    },
+    title: {
+      color: "white",
+      fontSize: 30,
+      marginTop: 50,
+      fontWeight: "200",
+    },
+    card: {
+      backgroundColor: "white",
+      flex: 1,
+      width: width - 25,
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+      ...Platform.select({
+        ios: {
+          shadowColor: "rgb(50,50,50)",
+          shadowOpacity: 0.5,
+          shadowRadius: 5,
+          shadowOffset: {
+            height: -1,
+            width: 0,
+          },
+        },
+        android: {
+          elevation: 3,
+        },
+      }),
+    },
+    input: {
+      padding:20,
+      borderBottomColor:"#bbb",
+      borderBottomWidth:2,
+      fontSize:25
+    },
+    todos:{
+      alignItems:"center"
+    }
+  });
--- ToDo.js
+++ ToDo.js
@@ -1,28 +1,122 @@
+  
 import React from 'react';
-import {View, Text, TouchableOpacity, StyleSheet, Dimensions} from  "react-native"
-import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
-
+import {View, Text, TouchableOpacity, StyleSheet, Dimensions, TextInput,} from  "react-native"
+import { faCheck,faPencilAlt, faTimesCircle  } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 const{width, height} = Dimensions.get("window");
-export default function ToDo(){
-  const [currentState, updateState] = React.useState();
-  var state = {
+
+export default class ToDo extends React.Component{
+  state = {
     isEdting:false,
-    isComplated:false,
+    isCompleted:false,
+    todoValue:"",
   }
-  console.log(state);
-  let _toggleComplate = ()=> {
-    console.log("abcdefg");
-    updateState(state.isComplated)
+  _toggleEdting = () => { 
+    let {text} = this.props;
+    let editCheck = false;
+    if(this.state.isEdting == false){
+      editCheck = true;
+    }else if(this.state.isEdting == true){
+      editCheck = false;
+    }
+    this.setState({
+      isEdting : editCheck,
+      todoValue : text,
+    })
   }
-  return(
-    <View style={styles.container}>
-      <TouchableOpacity onPress={_toggleComplate}>
-        <View style={styles.circle}>
+  _toggleComplete = () => {
+    let {text} = this.props;
+    let radioCheck = false;
+    if(this.state.isCompleted == false){
+      radioCheck = true
+    }else if(this.state.isCompleted==true){
+      radioCheck = false;
+    }
+    this.setState(()=>{
+      return (
+        {isCompleted : radioCheck}
+      )
+    })
+  }
+  _controllInput = () => {
+    let {text} = this.props;
+    this.setState(()=>{
+      return(
+        {todoValue : text}
+      )
+    })
+  }
+
+  render(){
+    const {isEdting,isCompleted} = this.state;
+    const {text} = this.props;
+    console.log("isCompleted: "+isCompleted)
+    return(
+      <View style={styles.container}>
+        <View style={styles.colum} >
+          <TouchableOpacity onPress={this._toggleComplete}>
+            <View style={[styles.circle, isCompleted ? styles.completedCircle : styles.uncomplatedCircle]}>
+            </View>
+          </TouchableOpacity>
+          {isEdting ? (
+            <TextInput 
+              style={[
+                styles.input, 
+                styles.text,
+                isCompleted ? styles.completedText : styles.uncompletedText
+              ]}
+              value={this.state.todoValue}
+              multiline={true}
+              returnKeyType={"done"}
+              onChangeText={this._controllInput}
+              onBlur={this._toggleEdting}
+            >
+            </TextInput>
+            ): (
+              <Text
+                style={[
+                  styles.text,
+                  isCompleted ? styles.completedText : styles.uncompletedText
+                ]}
+              >{this.props.text}</Text>
+            )
+          }
         </View>
-      </TouchableOpacity>
-      <Text style={styles.text}>Hello I'm a ToDo</Text>
-    </View>
-  );
+        <View style={styles.colum}>
+          {isEdting ? (//수정할때 모드
+              <View style={styles.action}>
+                <TouchableOpacity onPress={this._toggleEdting}>
+                  <View style={styles.actionContainer}>
+                    <Text style={styles.actionText}>
+                      <FontAwesomeIcon icon={faCheck} size="lg" />
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            ) : (//수정하지 않을때 모드(기본상태)
+              <View style={styles.action}>
+                <TouchableOpacity onPress={this._toggleEdting}>
+                  <View style={styles.actionContainer}>
+                    <Text>
+                      <FontAwesomeIcon icon={faPencilAlt}  size="lg" />
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={this._toggleEdting}> 
+                  <View style={styles.actionContainer}>
+                    <Text>
+                      <FontAwesomeIcon icon={faTimesCircle}  size="lg" />
+                    </Text>
+                  </View>
+                </TouchableOpacity>
+              </View>
+            )
+          }
+        </View>
+      </View>
+    );
+  }
+
 }
 
 const styles= StyleSheet.create({
@@ -30,15 +124,17 @@ const styles= StyleSheet.create({
     width:width -50,
     borderBottomColor:"#bbb",
     borderBottomWidth:StyleSheet.hairlineWidth,
-    flexDirection:"row"
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"space-between"
   },
   circle:{
     width:30,
     height:30,
     borderRadius:15,
-    backgroundColor:"red",
+    borderColor:"red",
     borderWidth:10,
-    marginRight:20
+    marginRight:20,
   },
   completedCircle:{
     borderColor:"#bbb"
@@ -50,5 +146,28 @@ const styles= StyleSheet.create({
     fontWeight:"600",
     fontSize:20,
     marginVertical:20,
+  },
+  completedText:{
+    color:"#bbb",
+    textDecorationLine:"line-through"
+  },
+  uncompletedText:{
+    color:"#353839"
+  },
+  colum:{
+    flexDirection:"row",
+    alignItems:"center",
+    justifyContent:"space-between"
+  },
+  action:{
+    flexDirection:"row",
+  },
+  actionContainer:{
+    marginVertical:10,
+    marginHorizontal:10
+    
+  },
+  input:{
+    marginVertical:15,
   }
 })
